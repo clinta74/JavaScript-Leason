c4d2531872e49094a5046a058f5e17ae41ad148f
Clinton Andrews <clinta74@live.com>
2022-09-07 21:11:51 -0400

    week 4

---
+++ Rob/index.js
@@ -0,0 +1,14 @@
+// Exercise 1
+// Import the sumPersonsReduce function as the default export. Export the other function as named exports. Create a function called run that 
+// calls and console logs all of the results.
+
+// Exercise 2 
+// Change the imported sumPersonsReduce fucntion to just be sum.
+
+// Exercise 3
+// Find a library to take the place of the sumPersonsReduce and import it to use replace the imported sumPersonReduce.
+
+// Exercise 4
+// Convert the index.js, persons.js, object.js and sorting.js to typescript files (.ts) Add the typescipt compiler as
+// a development dependancy. Using typescript compiler out put new JS files to a folder called dist and then with node
+// run the index.js file
